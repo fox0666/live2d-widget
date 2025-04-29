359f879cbc6509913d4b3929fb02a550cd236f09
Fox <209303911+fox0666@users.noreply.github.com>
2025-04-29 10:33:24 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fox0666/live2d_apiA/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/fox0666/live2d_apiA@latest/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
